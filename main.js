372e37df30cfa0baafca23c7a0f60206536b5f6f
Natalia Carvalho <nataliia.fc@gmail.com>
2021-10-23 23:31:44 -0300

    Alteração do .js

--- main.js
+++ main.js
@@ -2,16 +2,6 @@ const LinksSocialMedia = {
     github: 'nataliafc',
     instagram: 'nataliafc_'
   }
-  
-  function changeSocialMediaLinks() {
-    for (let li of socialLinks.children) {
-      const social = li.getAttribute('class')
-  
-      li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-    }
-  }
-  
-  changeSocialMediaLinks()
 
 function getGitHubProfileInfos(){
     const url = `https://api.github.com/users/${LinksSocialMedia.github}`
@@ -21,7 +11,6 @@ function getGitHubProfileInfos(){
       .then(data => {
         userBio.textContent = data.bio
       })
-
 }
 
 getGitHubProfileInfos()
